e499f75d9d6b24db457457cce8e76c18bb84fd96
Sanawar_21 <sanawarsaeed@gmail.com>
2024-08-31 18:47:33 +0500

    snapshot before combining the Transaction classes

--- sanchain/models/transaction.py
+++ sanchain/models/transaction.py
@@ -2,90 +2,24 @@ import rsa
 import base64
 import json
 
-from ..utils import generate_uid
+from ..utils import generate_uid, CONFIG
 from .base import AbstractBroadcastModel, AbstractDatabaseModel
+from .utxo import UTXO
 
 
-class Transaction(AbstractDatabaseModel, AbstractBroadcastModel):
+class Transaction(AbstractBroadcastModel, AbstractDatabaseModel):
+    """Transaction that can be signed and will be broadcasted
+    to the mempool.
+    Use Transaction.unsigned() to create a new unsigned transaction.
     """
-    A transaction between two accounts.
-    Use Transaction.unsigned() to create a new unsigned transaction. 
-    """
 
-    def __init__(self, uid, sender: rsa.PublicKey, receiver: rsa.PublicKey, amount: float, signature: bytes) -> None:
+    def __init__(self, uid: int, sender: rsa.PublicKey, receiver: rsa.PublicKey, amount: float, utxos: list[UTXO], signature: bytes) -> None:
         self.uid = uid
         self.sender = sender
         self.receiver = receiver
         self.amount = amount
+        self.utxos = utxos
         self.signature = signature
-
-    @classmethod
-    def unsigned(cls, sender: rsa.PublicKey, receiver: rsa.PublicKey, amount: float):
-        return cls(generate_uid(), sender, receiver, amount, b'')
-
-    def sign(self, private_key: rsa.PrivateKey):
-        self.signature = rsa.sign(
-            json.dumps(self.to_json()).encode(),
-            private_key,
-            'SHA-256'
-        )
-
-    def verify(self):
-        try:
-            data = self.to_json()
-            # remove signature
-            del data['signature']
-
-            rsa.verify(
-                data.encode(),
-                self.signature,
-                self.sender
-            )
-        except (rsa.VerificationError, KeyError):
-            return False
-        else:
-            return True
-
-    def to_json(self):
-        return {
-            'type': self.model_type,
-            'uid': self.uid,
-            'sender': base64.b64encode(self.sender.save_pkcs1("DER")).decode(),
-            'receiver': base64.b64encode(self.receiver.save_pkcs1("DER")).decode(),
-            'amount': self.amount,
-            'signature': base64.b64encode(self.signature).decode(),
-        }
-
-    @classmethod
-    def from_json(cls, json_data):
-        transaction = cls(
-            rsa.PublicKey.load_pkcs1(base64.b64decode(
-                json_data['sender']), format="DER"),
-            rsa.PublicKey.load_pkcs1(base64.b64decode(
-                json_data['receiver']), format="DER"),
-            json_data['amount'],
-            base64.b64decode(json_data['signature'])
-        )
-        transaction.uid = json_data['uid']
-        return transaction
-
-    def as_db_row(self):
-        return (
-            self.uid,
-            self.sender.save_pkcs1("DER"),
-            self.receiver.save_pkcs1("DER"),
-            self.amount,
-            self.signature
-        )
-
-    @classmethod
-    def from_db_row(cls, row):
-        return cls(
-            rsa.PublicKey.load_pkcs1(row[1], format="DER"),
-            rsa.PublicKey.load_pkcs1(row[2], format="DER"),
-            row[3],
-            row[4]
-        )
 
     @property
     def db_columns(self):
@@ -97,11 +31,135 @@ class Transaction(AbstractDatabaseModel, AbstractBroadcastModel):
             ('signature', 'BLOB'),
         ]
 
+    @classmethod
+    def unsigned(cls, sender: rsa.PublicKey, receiver: rsa.PublicKey, amount: float, utxos: list[UTXO]):
+        return cls(generate_uid(), sender, receiver, amount, utxos, b'')
 
-class ValidTransaction(Transaction):
-    """A transaction that has been validated by a miner and added to 
-    a block. It contains additional information about the block it is in.
-    """
-    # TODO: Add spent UTXOs
-    # TODO: Remove spent UTXOs from the sender on the database
-    # TODO: Add newly created UTXOs for sender, receiver and miner
+    @classmethod
+    def from_json(cls, json_data):
+        return cls(
+            json_data['uid'],
+            rsa.PublicKey.load_pkcs1(base64.b64decode(
+                json_data['sender']), format="DER"),
+            rsa.PublicKey.load_pkcs1(base64.b64decode(
+                json_data['receiver']), format="DER"),
+            json_data['amount'],
+            [UTXO.from_json(utxo) for utxo in json_data['utxos']],
+            base64.b64decode(json_data['signature'])
+        )
+
+    def to_json(self):
+        return {
+            'type': self.model_type,
+            'uid': self.uid,
+            'sender': base64.b64encode(self.sender.save_pkcs1("DER")).decode(),
+            'receiver': base64.b64encode(self.receiver.save_pkcs1("DER")).decode(),
+            'amount': self.amount,
+            'signature': base64.b64encode(self.signature).decode(),
+            'utxos': [utxo.to_json() for utxo in self.utxos]
+        }
+
+    def sign(self, private_key: rsa.PrivateKey):
+        data = self.to_json()
+        del data['signature']
+        self.signature = rsa.sign(
+            json.dumps(data).encode(),
+            private_key,
+            'SHA-256'
+        )
+
+
+class VerifiedTransaction(AbstractBroadcastModel, AbstractDatabaseModel):
+    """Use VerifiedTransaction.from_transaction() to verify a transaction from the mempool."""
+
+    def __init__(self, uid: int, sender: rsa.PublicKey, receiver: rsa.PublicKey, amount: float, utxos: list[UTXO], signature: bytes) -> None:
+        self.uid = uid
+        self.sender = sender
+        self.receiver = receiver
+        self.amount = amount
+        self.utxos = utxos
+        self.signature = signature
+
+        self.nascent_utxos = []
+        self.__is_verified = False
+
+    @classmethod
+    def from_json(self):
+        return {super().from_json()}
+
+    @classmethod
+    def unsigned(cls, sender: rsa.PublicKey, receiver: rsa.PublicKey, amount: float, utxos: list[UTXO]):
+        return cls(generate_uid(), sender, receiver, amount, utxos, b'')
+
+    def to_json(self):
+        return {
+
+        }
+
+    def sign(self, private_key: rsa.PrivateKey):
+        self.signature = rsa.sign(
+            json.dumps(self.to_json()).encode(),
+            private_key,
+            'SHA-256'
+        )
+
+    def verify(self):
+        """Verify the transaction and its utxos."""
+        try:
+            # verify the transaction itself
+            data = self.to_json()
+
+            # remove signature
+            del data['signature']
+
+            rsa.verify(
+                data.encode(),
+                self.signature,
+                self.sender
+            )
+
+            # verify the utxos
+            for utxo in self.utxos:
+                assert utxo.verification_key == rsa.compute_hash(
+                    self.sender, "SHA-256")
+
+            # verify the balance
+            assert sum([utxo.value for utxo in self.utxos]) >= (
+                self.amount + self.amount * CONFIG.miner_fees), "Insufficient balance"
+
+        except (rsa.VerificationError, KeyError, AssertionError):
+            return False
+        else:
+            self.__is_verified = True
+            return True
+
+    def execute(self, miner: rsa.PublicKey):
+        """Complete a transaction by creating new outputs as NascentUTXOs.
+        This method should be called after the transaction has been verified.
+        """
+        assert self.__is_verified
+
+        self.nascent_utxos = [
+            UTXO.nascent(  # receiver's utxo
+                rsa.compute_hash(self.receiver, "SHA-256"),
+                self.amount,
+                0
+            ),
+            UTXO.nascent(  # miner's fees
+                rsa.compute_hash(miner, "SHA-256"),
+                self.amount * CONFIG.miner_fee,
+                1
+            ),
+        ]
+
+        # return change, if any
+        change = self.amount - sum([utxo.value for utxo in self.nascent_utxos])
+
+        if change > 0:
+            self.nascent_utxos.append(  # sender's change
+                UTXO.nascent(
+                    rsa.compute_hash(self.sender, "SHA-256"),
+                    change,
+                    2
+                )
+            )
