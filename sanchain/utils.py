a040b0e6c9ae256cadc196bf11a84efc8fe2dbe5
Sanawar_21 <sanawarsaeed@gmail.com>
2024-08-31 18:14:38 +0500

    ensure uniqueness in uid generation

--- sanchain/utils.py
+++ sanchain/utils.py
@@ -1,7 +1,23 @@
 import time
 import random
 
+from models.config import SanchainConfig
 
-def generate_uid():
-    return int(str(int(time.time_ns() / 1_000_000)) +
-               str(random.randint(0, 1_000)))
+CONFIG = SanchainConfig.load_local()
+
+
+class UIDGenerator:
+    last_uid = 0
+
+    def get(self):
+        while True:
+            uid = int(str(int(time.time_ns() / 1_000_000)) +
+                      str(random.randint(100, 1_000)))
+            if uid != self.last_uid:
+                self.last_uid = uid
+                return uid
+
+
+__uidg = UIDGenerator()
+
+generate_uid = __uidg.get
